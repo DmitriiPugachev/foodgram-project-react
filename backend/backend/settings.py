526e7a214210b8f05346a6db0b7f19bd7852368e
DmitriiPugachev <pugachev.feddebate@gmail.com>
2021-12-05 15:23:15 +0100

    PAGE_SIZE changed.

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -137,6 +137,6 @@ REST_FRAMEWORK = {
          "rest_framework.authentication.TokenAuthentication",
     ],
     "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.PageNumberPagination",
-    "PAGE_SIZE": 20,
+    "PAGE_SIZE": 6,
     "DEFAULT_FILTER_BACKENDS": ["django_filters.rest_framework.DjangoFilterBackend"],
 }
